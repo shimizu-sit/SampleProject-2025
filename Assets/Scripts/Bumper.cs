5f4d352fc99031fe9a74f01de6f229acd5344056
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2025-03-16 01:30:04 +0900

    Update Bumper.cs

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 public class Bumper : MonoBehaviour
 {
   // float型でbounce変数を宣言し10を代入
-  public float bounce = 10;
+  public float bounce = 10f;
+  
+  void OnCollisionEnter(Collision collision)
+  {
+    // 衝突した相手オブジェクトのRigidbodyを取得
+    Rigidbody rb = collision.rigidbody;
 
-    void OnCollisionEnter(Collision collision)
-    {
-        // 衝突した相手オブジェクトのRigidbodyに力を加える
-        // x軸方向にはbounceの力を，Y軸方向にはbounce/4の力を，z軸方向には0の力を加える
-        // 力を瞬間的に加えるのでForceModeをImpulseに設定
-        collision.rigidbody.AddForce(bounce, bounce/4, 0f, ForceMode.Impulse);
+    if(rb != null){
+      // 衝突した相手オブジェクトのRigidbodyに力を加える
+      // x軸方向には0の力を，Y軸方向にはbounce/4の力を，z軸方向にはbounceの力を加える
+      // 力を瞬間的に加えるのでForceModeをImpulseに設定
+      collision.rigidbody.AddForce(0f, bounce/4, bounce, ForceMode.Impulse);
     }
+  }
 }
